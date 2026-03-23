4981db98fecc2b9aa26c46e5197714e4da570faf
smusa10 <s.musa10@hotmail.com>
2026-03-23 08:20:11 +0000

    finished the webcam classifier by adding normalisation to make the predictions more accurate.

--- classifier.py
+++ classifier.py
@@ -9,33 +9,50 @@ class_names = [line.split()[1] for line in open('labels.txt', 'r').readlines()]
 # Start the webcam
 cam = cv2.VideoCapture(0)
 
-# Process frames (using while loops)
+# Keep the loop while the webcam is opened
 while cam.isOpened():
-# Take the webcam's image
+# Read one frame and check if it works, if not loop breaks
     ret, img = cam.read()
-# resize the image
+    if not ret:
+        break
+
+# resize the frame to the size the model expects
     img = cv2.resize(img,(224, 224), interpolation = cv2.INTER_AREA)
+
 # Flip the image vertically
     flipped_img = cv2.flip(img, 1)
-# Showing the image in a window
-    cv2.imshow('flipped img', flipped_img)
+
 # Converts the image into the right format, data type and shape that the model will expect.
-    img = np.asarray(img, dtype="float32").reshape(1, 224, 224, 3)
-# Normalise the image
-    img_normalised = cv2.normalize(img, None, 0, 255, cv2.NORM_MINMAX)
-# The model predicts
-    prediction = model.predict(img_normalised)
+    img = np.asarray(flipped_img, dtype="float32").reshape(1, 224, 224, 3)
+
+# Normalise the image array for the model to improve prediction accuracy
+    img = (img / 127.5) - 1
+
+# The model makes predictions
+    prediction = model.predict(img)
     print(prediction)
 
-# Get labels
+# Automate the prediction so that python finds the biggest number in the index
+    index = np.argmax(prediction[0])
 
-# Listen to the keyboard for presses
-    keyboard_input = cv2.waitKey(2)
-# 27 is the ASCII for the esc key on your keyboard
-# This line stops the webcam when you press esc key
+# Show the results of the prediction with the correct label
+    print(class_names[index])
+
+# Need to put label on the image (webcam), slightly changed the positioning of the text away from the centre of the frame.
+    cv2.putText(flipped_img, class_names[index], (10,30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+
+# Show the image on the webcam which is separate from what the model sees.
+    cv2.imshow('Image', flipped_img)
+
+# Need to keep the image on the screen.
+    keyboard_input = cv2.waitKey(1)
+
+# Need to press esc to exit the loop
     if keyboard_input == 27:
         break
 
-# 
+# Release the webcam
 cam.release()
+
+# Completely close all opened windows.
 cv2.destroyAllWindows()
